e47a401a5584cfb81bbe68788787599ff6ab943d
mishobo <60912349+mishobo@users.noreply.github.com>
2022-09-04 13:05:31 +0300

    blog

--- src/main/kotlin/com/husseinabdallah/blog/Entities.kt
+++ src/main/kotlin/com/husseinabdallah/blog/Entities.kt
@@ -1,28 +1,27 @@
 package com.husseinabdallah.blog
 
-import toSlug
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 class Article (
-    @Id @GeneratedValue
-    var id: Long? = null,
     var title: String,
     var headline: String,
     var content: String,
     @ManyToOne
     var author: User,
     var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null
 
     )
 
 @Entity
 class User(
-    @Id @GeneratedValue
-    var id: Long? = null,
     var login: String,
     var firstname: String,
     var lastname: String,
     var description: String? = null,
+    @Id @GeneratedValue
+    var id: Long? = null
     )
--- src/main/kotlin/com/husseinabdallah/blog/Extensions.kt
+++ src/main/kotlin/com/husseinabdallah/blog/Extensions.kt
@@ -1,3 +1,5 @@
+package com.husseinabdallah.blog
+
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatterBuilder
 import java.time.temporal.ChronoField
